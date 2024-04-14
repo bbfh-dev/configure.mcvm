6ab28c5f932699d2317a7b9c4865f370469bb8a6
BubbleFish <daforsastudia@gmail.com>
2024-04-14 12:30:19 +0300

    feat: Add --icons flag

--- cli/flags.go
+++ cli/flags.go
@@ -1,5 +1,6 @@
 package cli
 
 var Flags struct {
-	Log string `short:"l" long:"log" description:"Path to log file"`
+	Log   string `short:"l" long:"log" description:"Path to log file"`
+	Icons bool   `short:"i" long:"icons" description:"Enable icons in TUI"`
 }
